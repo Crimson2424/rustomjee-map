8b6c8333ca329d18ea1b0bda5ef5312e86153412
Crimson2424 <priyankmiles@gmail.com>
2025-11-19 13:20:29 +0530

    Collapsed Leva by default

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -9,48 +9,47 @@ import { LuHotel } from "react-icons/lu";
 import { FaRegHospital } from "react-icons/fa";
 import { PiHospitalLight, PiMapPinSimpleArea } from "react-icons/pi";
 import { IoSchoolOutline } from "react-icons/io5";
-import { Environment, Preload } from "@react-three/drei";
+import { Environment, Preload, useProgress } from "@react-three/drei";
 import Loader from "./Loader";
 import { PathsProvider } from "./PathsContext";
 import UIButtons from "./UiButtons";
+import { Leva } from "leva";
 
 const Experience = () => {
+  const { active } = useProgress();
 
   return (
     <>
-    <PathsProvider>
-    <UIButtons/>
+      <PathsProvider>
+        <UIButtons />
+        <Leva collapsed />
 
-      <div className="h-screen w-screen">
-      <Loader /> {/* minimal overlay loader */}
-        <Canvas
-          camera={{
-            position: [80, 120, 200],
-            fov: 50,
-            near: 10,
-            far: 20000,
-          }}
-          
-          shadows
+        <div className="h-screen w-screen">
+          <Loader /> {/* minimal overlay loader */}
+          <Canvas
+            camera={{
+              position: [80, 120, 200],
+              fov: 50,
+              near: 10,
+              far: 20000,
+            }}
+            shadows
           >
-          <color attach="background" args={["#7fa4c9"]} />
-          <fog attach={"fog"} args={["#7fa4c9", 2000, 6000]} />
-          {/* <HorizontalFog
+            <color attach="background" args={["#7fa4c9"]} />
+            <fog attach={"fog"} args={["#7fa4c9", 2000, 6000]} />
+            {/* <HorizontalFog
             color="#a5b9c7"
             density={0.0005}
             heightFalloff={0.001}
             waterHeight={0} // your ocean Y position
             /> */}
-          <Environment preset="city" />
-          <Suspense fallback={null}>
-          
-          <Scene />
-          <Preload all />
-        </Suspense>
-        </Canvas>
-        
-
-        {/* <div className="w-fit fixed bg-[#F5F7FA] bottom-4 left-1/2 -translate-x-1/2 flex gap-3 p-2 py-3 items-center shadow-lg rounded-lg">
+            <Environment preset="city" />
+            <Suspense fallback={null}>
+              <Scene />
+              <Preload all />
+            </Suspense>
+          </Canvas>
+          {/* <div className="w-fit fixed bg-[#F5F7FA] bottom-4 left-1/2 -translate-x-1/2 flex gap-3 p-2 py-3 items-center shadow-lg rounded-lg">
           {categories.map((category) => {
             const Icon = category.icon;
             const isActive = selectedCategory === category.id;
@@ -77,7 +76,7 @@ const Experience = () => {
             </p>
           </div>
         )} */}
-      </div>
+        </div>
       </PathsProvider>
     </>
   );
